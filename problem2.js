09346690ffe0985c9d30db288f2a0ad96b8dfddb
JoniTalukder <jonictg7@gmail.com>
2020-04-07 15:35:07 +0600

    problem2 added

---
+++ problem2.js
@@ -0,0 +1,14 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+var n = 5;
+for(var i = 1; i <= 5; i++){
+    result = ''
+    for(var j = 1; j <= n; j++){
+        result += '*' + ' '
+    }
+    console.log(result);
+}
